074e849b988d5190ee6e4a33a4da68c9d3d5e68a
Catur Setyono <catursetyono542@gmail.com>
2025-11-19 11:59:16 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,21 @@
 <?php
 
+use App\Http\Controllers\MahasiswaController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\JenisBarangController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Routes untuk CRUD Jenis Barang
-Route::resource('jenis-barang', JenisBarangController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Routes untuk Mahasiswa (membutuhkan authentication)
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [MahasiswaController::class, 'dashboard'])->name('dashboard');
+    Route::post('/mahasiswa/store', [MahasiswaController::class, 'store'])->name('mahasiswa.store');
+    Route::get('/mahasiswa/{mahasiswa}/edit', [MahasiswaController::class, 'edit'])->name('mahasiswa.edit');
+    Route::put('/mahasiswa/{mahasiswa}/update', [MahasiswaController::class, 'update'])->name('mahasiswa.update');
+    Route::delete('/mahasiswa/{mahasiswa}/destroy', [MahasiswaController::class, 'destroy'])->name('mahasiswa.destroy');
+});
