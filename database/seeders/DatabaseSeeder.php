ed7ef078d7207ea8cac39dea174ccfb55e9d2832
Catur Setyono <catursetyono542@gmail.com>
2025-12-20 02:36:15 +0700

    chore(update): database/seeders/DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,13 +3,12 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
@@ -17,9 +16,22 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        // Seed Users
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        User::factory()->create([
+            'name' => 'Admin Panuntun',
+            'email' => 'admin@panuntun.com',
+        ]);
+
+        // Seed Jenis Barang dan Barang
+        $this->call([
+            DataSeeder::class,
+        ]);
+
+        // All data seeding handled by DataSeeder
     }
 }
